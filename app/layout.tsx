f252f09c588fd38c0821d0332edb6e81ecc77ac3
Bridger Tower <bridgertower@gmail.com>
2023-10-25 10:28:46 -0600

    m-auto layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
       className="w-screen dark:bg-neutral-700 dark:text-neutral-50 font-light"
       lang="en"
     >
-      <body className={`p-6 md:p-12 max-w-screen-2xl ${inter.className}`}>
+      <body
+        className={`p-6 md:p-12 max-w-screen-2xl m-auto ${inter.className}`}
+      >
         {children}
       </body>
     </html>

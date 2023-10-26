1a3537bdfd68ef06ba0f4ac1fc629623f1107a2d
Bridger Tower <bridgertower@gmail.com>
2023-10-26 14:18:33 -0600

    complete one

---
+++ app/playground/page.tsx
@@ -0,0 +1,13 @@
+import One from "@/components/type/hero/one";
+
+export default function Playground() {
+  return (
+    <section className="w-screen overflow-scroll fixed top-0 left-0 right-0 h-screen">
+      <One></One>
+
+      <div className="p-12">
+        <p>This is an example second section.</p>
+      </div>
+    </section>
+  );
+}
--- components/type/hero/one.tsx
+++ components/type/hero/one.tsx
@@ -1,12 +1,41 @@
+import Image from "next/image";
+import { HamburgerMenuIcon, ArrowDownIcon } from "@radix-ui/react-icons";
+
+// Import your hero image here
+import Placeholder from "@/public/placeholder.jpg";
+
 const One = () => {
   return (
-    <div className="h-screen w-screen max-w-full max-h-full flex flex-col gap-8 p-8 items-center justify-center bg-neutral-400">
-      <h1 className="text-6xl">One</h1>
-      <p>This is a component</p>
-      <a className="px-4 py-2 dark:bg-neutral-500 bg-neutral-300" href="/">
-        This is a test!
-      </a>
-    </div>
+    <section className="h-screen w-screen max-w-full grid gap-12 py-12 px-12 max-h-[1080px] dark:text-white bg-neutral-100 dark:bg-neutral-800">
+      <div className="w-full grid grid-cols-3">
+        <h1 className="text-6xl font-light uppercase">One</h1>
+        <h2 className="text-2xl font-extralight">
+          This is a marketing website component library using NextJS and
+          Tailwind CSS.
+        </h2>
+        <div className="flex justify-end gap-4">
+          <a
+            className="px-4 block py-2 transition-all hover:-mt-1 hover:mb-1 focus:scale-105 w-fit h-fit text-black dark:bg-neutral-300 bg-neutral-300"
+            href="#"
+          >
+            Get Started
+          </a>
+          <a
+            className="px-4 flex gap-2 items-center py-2 transition-all hover:-mt-1 hover:mb-1 focus:scale-105 w-fit h-fit dark:bg-neutral-500 bg-neutral-300"
+            href="#"
+          >
+            Menu <HamburgerMenuIcon />
+          </a>
+        </div>
+      </div>
+      <div className="overflow-hidden">
+        <Image objectFit="cover" src={Placeholder} alt=""></Image>
+      </div>
+      <div className="flex gap-2 items-center">
+        <p>Scroll to See More </p>
+        <ArrowDownIcon className="animate-pulse" />
+      </div>
+    </section>
   );
 };
 

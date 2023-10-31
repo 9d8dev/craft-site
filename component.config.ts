2cd1ab6a0006b166994c175ab9454fa0cdab9177
Bridger Tower <bridgertower@gmail.com>
2023-10-31 13:29:35 -0600

    component 003

--- component.config.ts
+++ component.config.ts
@@ -1,14 +1,17 @@
 // Component Imports
 import One from '@/components/type/hero/one';
 import Two from '@/components/type/hero/two';
+import Three from '@/components/type/hero/three';
 
 // Component Code Imports
 import oneCode from '!!raw-loader!@/components/type/hero/one';
 import twoCode from '!!raw-loader!@/components/type/hero/two';
+import threeCode from '!!raw-loader!@/components/type/hero/three';
 
 // Component Image Imports
 import oneImage from '@/public/example/one.png';
 import twoImage from '@/public/example/two.png';
+import threeImage from '@/public/example/three.png';
 
 export const components = [
 	{
@@ -25,6 +28,14 @@ export const components = [
 		image: twoImage,
 		component: Two,
 		componentCode: twoCode,
+		description: 'This is a test component for the setup of the Craft UI library'
+	},
+	{
+		name: 'Third Component',
+		type: 'Hero',
+		image: threeImage,
+		component: Three,
+		componentCode: threeCode,
 		description: 'This is a test component for the setup of the Craft UI library'
 	}
 ];

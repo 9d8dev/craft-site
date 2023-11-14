2a75876beb8c5ea1c5da4ba409cff0103f03fe82
Bridger Tower <bridgertower@gmail.com>
2023-11-14 15:32:19 -0700

    add container and section

---
+++ components/layout/main.tsx
@@ -0,0 +1,9 @@
+interface MainProps {
+  children: React.ReactNode;
+}
+
+const Main: React.FC<MainProps> = ({ children }) => {
+  return <main className="m-auto">{children}</main>;
+};
+
+export default Main;
---
+++ components/layout/section.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode } from "react";
+
+type SectionProps = {
+  children: ReactNode;
+  className?: string;
+  bgColor?: string;
+  isAlt?: boolean;
+};
+
+const Section = ({
+  children,
+  isAlt,
+  className,
+  bgColor = "bg-secondary-300 dark:bg-secondary-900",
+}: SectionProps) => {
+  const backgroundColor = isAlt
+    ? "bg-secondary-200 dark:bg-secondary-700"
+    : bgColor;
+
+  return (
+    <section
+      className={`${className} m-auto items-center p-6 text-sm md:p-12 ${backgroundColor}`}
+    >
+      {children}
+    </section>
+  );
+};
+
+export default Section;

8493f0bdb21763f8bfc75fc528419794158b2127
Bridger Tower <bridgertower@gmail.com>
2024-01-22 09:18:01 -0700

    add post-card

--- components/type/faqs/one.tsx
+++ components/type/faqs/one.tsx
@@ -1,63 +1,66 @@
-// Layout
 import * as Craft from "@/components/craft/layout";
-import Balancer from "react-wrap-balancer";
 
-// Icons
-import { HomeIcon, HeartIcon, StarIcon } from "@radix-ui/react-icons";
+import { ArrowTopRightIcon } from "@radix-ui/react-icons";
 
-type FeatureText = {
-  icon: JSX.Element;
-  title: string;
-  description: string;
-};
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
-const featureText: FeatureText[] = [
+const content: faqProps[] = [
   {
-    icon: <StarIcon className="w-6 h-6" />,
-    title: "Veteran Disability",
-    description:
-      "Many veterans bear the mental and physical scars sustained during their service. Veteran disability benefits provides veterans with compensation for their sacrifice.",
+    question: "What is All Veteran and how can it help me?",
+    answer:
+      "All Veteran is a dedicated platform that offers comprehensive resources and information on veteran benefits. Whether you're looking to understand your entitlements, seeking healthcare support, or need assistance with post-military career opportunities, our platform guides you through the available benefits and how to access them.",
   },
   {
-    icon: <HomeIcon className="w-6 h-6" />,
-    title: "Housing Benefits",
-    description:
-      "Whether it's moving into a new home, or remodeling a current home to be better suited to life with a disability, there are mortgage and loan services that only veterans can access.",
+    question: "Are the resources on All Veterans free to access?",
+    answer:
+      "Yes, All Veteran is committed to supporting the veteran community by providing free access to all our informational resources. Our goal is to ensure every veteran is well-informed about the benefits they've earned through their service.",
   },
   {
-    icon: <HeartIcon className="w-6 h-6" />,
-    title: "VA Healthcare",
-    description:
-      "Service to our nation has guaranteed many veterans access to quality healthcare for the rest of their lives. However, knowing what is covered and where to get medical help can be tough.",
+    question:
+      "How can I ensure I'm eligible for the benefits listed on All Veteran?",
+    answer:
+      "While All Veteran provides a comprehensive overview of available benefits, individual eligibility can vary based on service duration, type of discharge, and other criteria. We recommend consulting with a Veterans Service Officer (VSO) or the VA directly to verify your specific eligibility.",
+  },
+  {
+    question: "Is All Veteran associated with the VA?",
+    answer:
+      "We are not associated with the VA. Rather, we offer high-quality free resources to veterans and friends of veterans to help them get the benefits they've earned.",
   },
 ];
 
-const Feature = () => {
+const FAQ = () => {
   return (
-    <Craft.Section className="border-b">
-      <Craft.Container className="not-prose">
-        <div className="flex flex-col gap-6">
-          <h3 className="text-4xl">
-            <Balancer>
-              Uncover the benefits you&apos;ve earned through service to our
-              nation
-            </Balancer>
-          </h3>
+    <Craft.Section>
+      <Craft.Container>
+        <div className="flex flex-col not-prose gap-6">
+          <h3 className="text-4xl">Frequently Asked Questions</h3>
           <h4 className="text-2xl font-light opacity-70">
-            <Balancer>
-              Your tireless and valiant service has not gone unnoticed. Our
-              great nation recognizes your sacrifice by providing you and other
-              heroes with exclusive veteran benefits.
-            </Balancer>
+            Can&apos;t find the answer you&apos;re looking for? Reach out to our
+            customer support team.
           </h4>
-
-          <div className="grid md:grid-cols-3 mt-6 gap-6 md:mt-12">
-            {featureText.map(({ icon, title, description }, index) => (
-              <div className="grid gap-4" key={index}>
-                {icon}
-                <h4 className="text-xl text-primary">{title}</h4>
-                <p className="text-base opacity-75">{description}</p>
-              </div>
+          <div className="mt-6 md:mt-12 flex flex-col gap-6">
+            {content.map((item, index) => (
+              <Accordion key={index} type="single" collapsible>
+                <AccordionItem value={item.question}>
+                  <AccordionTrigger>{item.question}</AccordionTrigger>
+                  <AccordionContent className="text-base md:w-3/4">
+                    {item.answer}
+                    {item.link && (
+                      <a
+                        href={item.link}
+                        className="opacity-60 mt-2 hover:opacity-100 transition-all flex items-center"
+                      >
+                        Learn more <ArrowTopRightIcon className="ml-1" />
+                      </a>
+                    )}
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
             ))}
           </div>
         </div>
@@ -66,4 +69,4 @@ const Feature = () => {
   );
 };
 
-export default Feature;
+export default FAQ;
